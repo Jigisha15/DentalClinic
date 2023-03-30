1b0c41ed313351df255a3b6f75f7386f5e5686ae
Jigisha Ghanekar <jigishamanohar18@gmail.com>
2023-03-30 21:39:41 +0530

    this is commit

---
+++ Project/dentApp/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+from dentApp.forms import *
+from django.contrib.auth import views as auth_views
+# from .forms import LoginForm
+from dentApp.views import AppointmentTemplateView, ManageAppointmentTemplateView
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    # this is for sending appointment
+    path('appointment/', AppointmentTemplateView.as_view(), name="appointment"),
+    path('manageapp/', ManageAppointmentTemplateView.as_view(), name="manageapp"),
+    path('services/', views.services, name="services"),
+    path('blog/', views.blog, name="blog"),
+    path('contact/', views.contact, name="contact"),
+    path('contactelse/', views.contactelse, name="contactelse"),
+    path('gallery/', views.gallery, name="gallery"),
+    path('location/', views.location, name="location"),
+    path('tnc/', views.tnc, name="tnc"),
+    # this is for login of Patient 
+    path('login/', views.login, name="login"),
+    # this is for logout of Patient 
+    path('logout/', views.Logout, name="logout"),
+    # this is for signup of patient
+    path('signup/', views.PatientRegistrationView.as_view(), name="signup"),
+    # this is for profile of Patient 
+    path('profile/', views.profile, name="profile"),
+    path('team/', views.team, name="team"),
+    path('testimonials/', views.testimonials, name="testimonials"),
+]
